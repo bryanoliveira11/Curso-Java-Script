e3544092445fc57e5a0fa0fbe361b4a68168ce9f
bryan <bryan.oliveira12345@gmail.com>
2024-01-18 14:07:49 -0300

    configurando database, migration, e models com o Sequelize

--- API Rest/src/controllers/HomeController.js
+++ API Rest/src/controllers/HomeController.js
@@ -1,5 +1,16 @@
+import Student from '../models/Student';
+
 class HomeController {
-  index(req, res) {
+  async index(req, res) {
+    const newStudent = await Student.create({
+      name: 'test',
+      lastname: 'user',
+      email: 'testuser@gmail.com',
+      age: 44,
+      height: 100,
+      weight: 1.8,
+    })
+
     res.json({
       'controller working': true,
     });
