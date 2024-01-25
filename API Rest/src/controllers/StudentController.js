677df65add57a1a50de35d844f303b610d79dd09
bryan <bryan.oliveira12345@gmail.com>
2024-01-25 10:41:57 -0300

    Students CRUD

--- API Rest/src/controllers/StudentController.js
+++ API Rest/src/controllers/StudentController.js
@@ -10,6 +10,103 @@ class StudentController {
       return res.json(null);
     }
   }
+
+  async create(req, res) {
+    try {
+      const newStudent = await Student.create(req.body);
+      const { id, name, lastname, email, age, height, weight } = newStudent;
+
+      return res.json({ id, name, lastname, email, age, height, weight });
+    }
+    catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map(err => err.message)
+      });
+    }
+  };
+
+  async show(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(404).json({
+          errors: ['ID Inválido.']
+        })
+      };
+
+      const student = await Student.findByPk(req.params.id);
+
+      if (!student) {
+        return res.status(404).json({
+          errors: ['Aluno não Existe.']
+        })
+      };
+
+      const { id, name, lastname, email, age, height, weight } = student
+
+      return res.json({ id, name, lastname, email, age, height, weight });
+    }
+    catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map(err => err.message)
+      });
+    }
+  };
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(404).json({
+          errors: ['ID Inválido.']
+        })
+      };
+
+      const student = await Student.findByPk(req.params.id);
+
+      if (!student) {
+        return res.status(404).json({
+          errors: ['Aluno não Existe.']
+        })
+      };
+
+      const newData = await student.update(req.body);
+      const { id, name, lastname, email, age, height, weight } = newData
+
+      return res.json({ id, name, lastname, email, age, height, weight });
+    }
+    catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map(err => err.message)
+      });
+    }
+  };
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(404).json({
+          errors: ['ID Inválido.']
+        })
+      };
+
+      const student = await Student.findByPk(req.params.id);
+
+      if (!student) {
+        return res.status(404).json({
+          errors: ['Aluno não Existe.']
+        })
+      };
+
+      student.destroy();
+
+      return res.json(`Aluno ${student.name} ${student.lastname} Deletado com Sucesso.`)
+
+    }
+    catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map(err => err.message)
+      });
+    }
+  };
 }
 
 export default new StudentController();
--- API Rest/src/models/Student.js
+++ API Rest/src/models/Student.js
@@ -4,12 +4,65 @@ export default class Student extends Model {
   static init(sequelize) {
     super.init(
       {
-        name: Sequelize.STRING,
-        lastname: Sequelize.STRING,
-        email: Sequelize.STRING,
-        age: Sequelize.INTEGER,
-        height: Sequelize.FLOAT,
-        weight: Sequelize.FLOAT,
+        name: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [3, 255],
+              msg: 'Nome precisa ter entre 3 e 255 caracteres',
+            }
+          }
+        },
+        lastname: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [3, 255],
+              msg: 'Sobrenome precisa ter entre 3 e 255 caracteres',
+            }
+          }
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          unique: {
+            msg: 'Este Email já Está em Uso.'
+          },
+          validate: {
+            isEmail: {
+              msg: 'Email Inválido.'
+            },
+          }
+        },
+        age: {
+          type: Sequelize.INTEGER,
+          defaultValue: '',
+          validate: {
+            isInt: {
+              msg: 'Idade precisa ser um número inteiro.'
+            }
+          }
+        },
+        height: {
+          type: Sequelize.FLOAT,
+          defaultValue: '',
+          validate: {
+            isFloat: {
+              msg: 'Peso precisa ser um número inteiro ou de ponto flutuante.'
+            }
+          }
+        },
+        weight: {
+          type: Sequelize.FLOAT,
+          defaultValue: '',
+          validate: {
+            isFloat: {
+              msg: 'Altura precisa ser um número inteiro ou de ponto flutuante.'
+            }
+          }
+        },
       },
       {
         sequelize,
