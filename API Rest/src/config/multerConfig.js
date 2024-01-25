efdbfaa77d6f78a0ab9fc50a281a8a551045d338
bryan <bryan.oliveira12345@gmail.com>
2024-01-25 13:18:53 -0300

    Validando o tipo de arquivo

--- API Rest/src/config/multerConfig.js
+++ API Rest/src/config/multerConfig.js
@@ -4,6 +4,12 @@ import path from "path";
 const random = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpg') {
+      return cb(new multer.MulterError('Somente Arquivos png ou jpg não Permitidos.'));
+    }
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       // destino do upload
--- API Rest/src/controllers/PhotoController.js
+++ API Rest/src/controllers/PhotoController.js
@@ -1,7 +1,20 @@
+import multer from 'multer';
+import photoController from '../controllers/PhotoController';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('photo');
+
 class PhotoController {
   async create(req, res) {
     try {
-      return res.json(req.file);
+      return upload(req, res, (err) => {
+        if (err) {
+          res.status(400).json({
+            errors: [err.code],
+          });
+        }
+        return res.json(req.file);
+      });
     }
     catch (err) {
       return res.json(null);
