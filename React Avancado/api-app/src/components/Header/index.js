f18996e15e2bc05b8734072755bc3cfeff067142
bryan <bryan.oliveira12345@gmail.com>
2024-02-06 10:25:43 -0300

    Páginas e Rotas do app

--- React Avancado/api-app/src/components/Header/index.js
+++ React Avancado/api-app/src/components/Header/index.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { Nav } from './styled';
 
 export default function Header() {
-  // exampleReducer comes from rootReducer in store/modules/example
-  const btnClicked = useSelector((state) => state.exampleReducer.btnClicked);
-
   return (
     <Nav>
       <Link to="/" title="Home">
         <FaHome size={24} />{' '}
       </Link>
 
-      <Link to="/login" title="Login">
+      <Link to="/register" title="Register">
         <FaUserAlt size={24} />{' '}
       </Link>
 
-      <Link to="/logout" title="Logout">
+      <Link to="/login" title="Login">
         <FaSignInAlt size={24} />{' '}
       </Link>
-
-      {btnClicked ? 'Clicked' : 'Not Clicked'}
     </Nav>
   );
 }
