e8795e13e1ff19d7b1f575f56e56dc72355c1a62
bryan <bryan.oliveira12345@gmail.com>
2024-02-12 10:41:05 -0300

    page not found nas routes

--- React Avancado/react-router-dom-v6/src/main.tsx
+++ React Avancado/react-router-dom-v6/src/main.tsx
@@ -7,6 +7,7 @@ import { About } from './components/about'
 import { Menu } from './components/menu'
 import { Post } from './components/post'
 import { Redirect } from './components/redirect'
+import { NotFound } from './components/notFound'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -18,6 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='/redirect' element={<Redirect/>}/>
         <Route path='/posts/:id' element={<Post/>}/>
         <Route path='/posts' element={<Post/>}/>
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
