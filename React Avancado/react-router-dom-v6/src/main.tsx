0d4209f7437fa2c5d1b012933543f66794c9c867
bryan <bryan.oliveira12345@gmail.com>
2024-02-12 10:33:40 -0300

    Link, useParams e useSearchParams

--- React Avancado/react-router-dom-v6/src/main.tsx
+++ React Avancado/react-router-dom-v6/src/main.tsx
@@ -4,13 +4,18 @@ import './styles/global.css'
 import { Home } from './components/home'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import { About } from './components/about'
+import { Menu } from './components/menu'
+import { Post } from './components/post'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Menu/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='/posts/:id' element={<Post/>}/>
+        <Route path='/posts' element={<Post/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
