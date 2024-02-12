3bee8b7bd0d9a8a5d67ce170ddc5c9447e7b8bf8
bryan <bryan.oliveira12345@gmail.com>
2024-02-12 10:50:58 -0300

    Rotas aninhadas e outlet

--- React Avancado/react-router-dom-v6/src/components/post/index.tsx
+++ React Avancado/react-router-dom-v6/src/components/post/index.tsx
@@ -1,11 +1,10 @@
-import { useParams, useSearchParams } from "react-router-dom";
-
 export const Post = () => {
-    const params = useParams();
-    const {id} = params;
-    const [qs] = useSearchParams();
-
-    return <div>
-        <h1> Post {`Param : ${id}`} {`QS : ${qs.get('page')}`} </h1>
-    </div>
+    return (
+        <div>
+           Post
+           <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit eligendi nulla quas asperiores, veritatis delectus aliquid nihil dolores optio incidunt fugiat at, autem vel veniam in earum possimus explicabo? Assumenda.
+           </p>
+        </div>
+    )
 }
--- React Avancado/react-router-dom-v6/src/main.tsx
+++ React Avancado/react-router-dom-v6/src/main.tsx
@@ -5,9 +5,10 @@ import { Home } from './components/home'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import { About } from './components/about'
 import { Menu } from './components/menu'
-import { Post } from './components/post'
+import { Posts } from './components/posts'
 import { Redirect } from './components/redirect'
 import { NotFound } from './components/notFound'
+import { Post } from './components/post'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -17,10 +18,13 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/redirect' element={<Redirect/>}/>
-        <Route path='/posts/:id' element={<Post/>}/>
-        <Route path='/posts' element={<Post/>}/>
+        {/*<Route path='/posts/:id' element={<Posts/>}/>*/}
+        <Route path='/posts' element={<Posts/>}>
+          <Route path=':id' element={<Post/>}/>
+        </Route>
+        <Route path='/posts' element={<Posts/>}/>
         <Route path='*' element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
