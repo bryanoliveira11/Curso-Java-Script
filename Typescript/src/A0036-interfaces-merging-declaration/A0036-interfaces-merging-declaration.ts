b1c24f16001462caa5f7d54791bf4edbfd7d3eff
bryan <bryan.oliveira12345@gmail.com>
2024-02-27 11:45:47 -0300

    interfaces merging declaration

---
+++ Typescript/src/A0036-interfaces-merging-declaration/A0036-interfaces-merging-declaration.ts
@@ -0,0 +1,27 @@
+/* interfaces merging declaration */
+
+interface PersonMerge {
+  name: string;
+}
+
+/* merging */
+interface PersonMerge {
+  readonly lastname: string;
+}
+
+interface PersonMerge {
+  readonly addresses: readonly string[];
+}
+
+interface PersonMerge {
+  /* optional property - ? */
+  readonly age?: number;
+}
+
+const p: PersonMerge = {
+  name: 'name',
+  lastname: 'lastname',
+  addresses: ['address'],
+};
+
+console.log(p);
