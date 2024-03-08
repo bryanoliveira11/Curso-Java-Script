8a83500d1737d011b1b0b8446e9bbb6c25381072
bryan <bryan.oliveira12345@gmail.com>
2024-03-08 10:29:52 -0300

    Decorator Class

---
+++ Typescript/src/A0052-decorator-class/A0052-decorator-class.ts
@@ -0,0 +1,29 @@
+/* decorator class */
+
+@decorator
+export class Animal {
+  constructor(
+    public name: string,
+    public color: string,
+  ) {}
+}
+
+function decorator<T extends new (...args: any[]) => any>(target: T): T {
+  return class extends target {
+    name: string;
+    color: string;
+
+    constructor(...args: any[]) {
+      super(...args);
+      this.name = this.reverse(args[0]);
+      this.color = this.reverse(args[1]);
+    }
+
+    reverse(value: string): string {
+      return value.split('').reverse().join('');
+    }
+  };
+}
+
+const animal = new Animal('the name', 'brown');
+console.log(animal);
