7b15ce63e0645ae9050e91f24e4eaad360e42b96
bryan <bryan.oliveira12345@gmail.com>
2024-03-14 10:51:08 -0300

    Liskov Substitution Principle

---
+++ S.O.L.I.D/src/A0003-Liskov-Substitution-Principle/classes/shopping-cart-legacy.ts
@@ -0,0 +1,70 @@
+/* Shopping Cart Without any Principle */
+
+type CartItem = { name: string; price: number };
+type OrderStatus = 'open' | 'closed';
+
+export class ShoppingCartLegacy {
+  private readonly _items: CartItem[] = [];
+  private _orderStatus: OrderStatus = 'open';
+
+  addItem(item: CartItem): void {
+    this._items.push(item);
+  }
+
+  removeItem(index: number): void {
+    this._items.splice(index, 1);
+  }
+
+  get items(): Readonly<CartItem[]> {
+    return this._items;
+  }
+
+  get orderStatus(): Readonly<OrderStatus> {
+    return this._orderStatus;
+  }
+
+  total(): number {
+    return +this._items
+      .reduce((value, next) => value + next.price, 0)
+      .toFixed(2);
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0;
+  }
+
+  sendMessage(msg: string): void {
+    console.log(msg);
+  }
+
+  saveOrder(): void {
+    console.log('Order Saved.');
+  }
+
+  clear(): void {
+    this._items.length = 0;
+  }
+
+  checkout(): void {
+    if (this.isEmpty()) {
+      console.log('Your Cart is Empty.');
+      return;
+    }
+
+    this._orderStatus = 'closed';
+    this.sendMessage(
+      `Your Order with a total of ${this.total()}$ was Received.`,
+    );
+    this.saveOrder();
+    this.clear();
+  }
+}
+
+const shoppingCart = new ShoppingCartLegacy();
+shoppingCart.addItem({ name: 'p1', price: 10 });
+shoppingCart.addItem({ name: 'p2', price: 20 });
+shoppingCart.addItem({ name: 'p3', price: 30 });
+shoppingCart.removeItem(1);
+console.log(shoppingCart);
+shoppingCart.checkout();
+console.log(shoppingCart);
