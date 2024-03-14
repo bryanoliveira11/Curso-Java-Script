cd7b0258a76916c56011946f7fd6340c8f14e72c
bryan <bryan.oliveira12345@gmail.com>
2024-03-14 10:37:10 -0300

    Open Closed Principle

---
+++ S.O.L.I.D/src/A0002-Open-Closed-Principle/index.ts
@@ -0,0 +1,28 @@
+/*
+Open closed principle
+"software entities (classes, modules, functions, etc.) should be open for extension, but closed for modification"
+*/
+
+import { Messaging } from './services/messaging';
+import { Order } from './classes/order';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import { NoDiscount } from './classes/discount';
+
+// const fiftypercentdiscount = new FiftyPercentDiscount();
+// const tenpercentdiscount = new TenPercentDiscount();
+const nodiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(nodiscount);
+const messaging = new Messaging();
+const persisntecy = new Persistency();
+
+const order = new Order(shoppingCart, messaging, persisntecy);
+
+shoppingCart.addItem(new Product('p1', 10));
+shoppingCart.addItem(new Product('p2', 20));
+shoppingCart.addItem(new Product('p3', 30));
+console.log(shoppingCart, order.orderStatus);
+console.log(shoppingCart.total(), shoppingCart.totalWithDiscount());
+order.checkout();
+console.log(shoppingCart, order.orderStatus);
