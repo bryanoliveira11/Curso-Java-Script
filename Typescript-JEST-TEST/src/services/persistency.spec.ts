fe28bce4762298dba84eae01488a36675d20be7e
bryan <bryan.oliveira12345@gmail.com>
2024-03-20 10:15:06 -0300

    Persistency tests

--- Typescript-JEST-TEST/src/services/persistency.spec.ts
+++ Typescript-JEST-TEST/src/services/persistency.spec.ts
@@ -1,13 +1,27 @@
-describe('testing something', () => {
-  it('should return 1', () => {
-    const number = 1;
-    expect(number).toBe(1);
-  });
-});
+import { Persistency } from './persistency';
 
-describe('testing something 2', () => {
-  test('should return my test', () => {
-    const name = 'my test';
-    expect(name).toBe('my test');
+describe('Persistency', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return undefined', () => {
+    // System Under Test
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Order Saved." ', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Order Saved.');
   });
 });
