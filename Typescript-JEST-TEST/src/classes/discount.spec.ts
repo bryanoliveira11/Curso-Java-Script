25e8dc10cb662e6c659b2bb0eb0ed1954f646fe2
bryan <bryan.oliveira12345@gmail.com>
2024-03-20 10:28:20 -0300

    Product tests

--- Typescript-JEST-TEST/src/classes/discount.spec.ts
+++ Typescript-JEST-TEST/src/classes/discount.spec.ts
@@ -1,7 +1,7 @@
-import { Product } from './product';
+import { Discount } from './discount';
 
-const createSut = (name: string, price: number) => {
-  return new Product(name, price);
+const createSut = () => {
+  return new Discount();
 };
 
 describe('Product', () => {
@@ -9,7 +9,7 @@ describe('Product', () => {
     jest.clearAllMocks();
   });
 
-  it('should have properties name and price', () => {
+  it('should return undefined', () => {
     const sut = createSut('product-test', 9.9);
     expect(sut).toHaveProperty('name', 'product-test');
     expect(sut.price).toBeCloseTo(9.9);
