17ab468323ace40aa73ae02fdc8a8612a709daa2
bryan <bryan.oliveira12345@gmail.com>
2024-03-25 10:43:11 -0300

    pomodoro timer initial component

---
+++ Typescript-React-Pomodoro-App/pomodoro-app/src/components/pomodoro-timer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useInterval } from '../hooks/useInterval';
+
+interface Props {
+  defaultPomodoroTime: number;
+}
+
+export function PomodoroTimer(props: Props): JSX.Element {
+  const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTime);
+
+  useInterval(() => {
+    setMainTime(mainTime - 1);
+  }, 1000);
+
+  return <div>olá mundo {mainTime}</div>;
+}
