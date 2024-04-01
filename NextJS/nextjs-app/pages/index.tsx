a1d7a68d4330a3b33d26f42ca86f6dbacb306b1c
bryan <bryan.oliveira12345@gmail.com>
2024-04-01 11:15:48 -0300

    Using getStaticProps from next to fetch api data

--- NextJS/nextjs-app/pages/index.tsx
+++ NextJS/nextjs-app/pages/index.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <h1>olá mundo</h1>;
+import { PostData } from '@/src/domain/posts/post';
+import { GetStaticProps } from 'next';
+
+const getPosts = async (): Promise<PostData[]> => {
+  const posts = await fetch('http://127.0.0.1:1337/api/posts/');
+  const jsonPosts = await posts.json();
+  return jsonPosts.data;
+};
+
+export type HomeProps = {
+  posts: PostData[];
+};
+
+export default function Home({ posts }: HomeProps) {
+  return (
+    <div>
+      {posts.map((post) => (
+        <h2 key={post.id}>{post.attributes.title}</h2>
+      ))}
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const posts = await getPosts();
+  return {
+    props: { posts },
+  };
+};
