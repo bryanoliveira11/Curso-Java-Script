ad8b64145324e30463090ceafec7ba494242741f
bryan <bryan.oliveira12345@gmail.com>
2024-04-08 11:11:05 -0300

    SITE_URL adjust

--- NextJS/nextjs-app/src/config/app-config.ts
+++ NextJS/nextjs-app/src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'http://127.0.0.1:1337/api';
 export const POSTS_URL_WITHOUT_POPULATE = `${API_URL}/posts`;
 export const POSTS_URL = `${API_URL}/posts?populate=deep`;
 export const SITE_NAME = 'Next JS Project';
-export const SITE_URL = '';
+export const SITE_URL = process.env.SITE_URL || 'http://127.0.0.1:3000';
