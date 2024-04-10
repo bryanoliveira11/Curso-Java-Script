05876642ded68646df139c2c6ffb325e8303f199
bryan <bryan.oliveira12345@gmail.com>
2024-04-10 10:37:11 -0300

    Pagination Part 2

--- NextJS/nextjs-app/pages/post/page/[...param].tsx
+++ NextJS/nextjs-app/pages/post/page/[...param].tsx
@@ -3,12 +3,16 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 import { getAllPosts } from '@/src/data/posts/get-all-posts';
 import HomePage from '@/src/containers/HomePage';
 import { useRouter } from 'next/router';
+import { PaginationData } from '@/src/domain/posts/pagination';
+import { countAllPosts } from '@/src/data/posts/count-posts';
 
 export type PageProps = {
   posts: PostData[];
+  category?: string;
+  pagination: PaginationData;
 };
 
-export default function Page({ posts }: PageProps) {
+export default function Page({ posts, pagination, category }: PageProps) {
   const router = useRouter();
 
   if (router.isFallback) return <div>Loading...</div>;
@@ -25,11 +29,40 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  console.log(ctx);
-  const posts = await getAllPosts(
-    'sort=id:desc&pagination[start]=0&pagination[limit]=10',
-  );
+  if (!ctx.params || !ctx.params.param) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const page = Number(ctx.params.param[0]);
+  const category = ctx.params.param[1] || '';
+  const postsPerPage = 3;
+  const startFrom = (page - 1) * postsPerPage;
+
+  const nextPage = page + 1;
+  const previousPage = page - 1;
+
+  const categoryQuery = category
+    ? `&filters[category][name][$eq]=${category}`
+    : '';
+
+  const urlQuery = `sort=id:desc&pagination[start]=${startFrom}&pagination[limit]=${postsPerPage}${categoryQuery}`;
+  const posts = await getAllPosts(urlQuery);
+
+  const numberOfPosts = Number(await countAllPosts(categoryQuery));
+
+  const pagination: PaginationData = {
+    nextPage,
+    previousPage,
+    numberOfPosts,
+    postsPerPage,
+    category,
+  };
+
+  console.log(urlQuery);
+
   return {
-    props: { posts },
+    props: { posts, pagination, category },
   };
 };
