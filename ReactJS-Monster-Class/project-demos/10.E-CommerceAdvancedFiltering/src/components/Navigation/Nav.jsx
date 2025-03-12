0c774a37fb0533355b1ffb8c4cfe32cd954c0580
bryan <bryan.oliveira12345@gmail.com>
2025-03-12 14:12:53 -0300

    Nav

--- ReactJS-Monster-Class/project-demos/10.E-CommerceAdvancedFiltering/src/components/Navigation/Nav.jsx
+++ ReactJS-Monster-Class/project-demos/10.E-CommerceAdvancedFiltering/src/components/Navigation/Nav.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import "./Nav.css";
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 
 export default function Nav() {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <div className="nav-container">
+        <input type="text" className="search-input" placeholder="Search..." />
+      </div>
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
+      </div>
+    </nav>
+  );
 }
