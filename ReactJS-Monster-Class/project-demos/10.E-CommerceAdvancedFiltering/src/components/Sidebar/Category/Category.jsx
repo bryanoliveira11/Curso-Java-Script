d8f219a92637a7a5e979a056b41ac1c75e8aca79
bryan <bryan.oliveira12345@gmail.com>
2025-03-13 14:00:13 -0300

    Category

--- ReactJS-Monster-Class/project-demos/10.E-CommerceAdvancedFiltering/src/components/Sidebar/Category/Category.jsx
+++ ReactJS-Monster-Class/project-demos/10.E-CommerceAdvancedFiltering/src/components/Sidebar/Category/Category.jsx
@@ -2,5 +2,31 @@ import React from "react";
 import "./Category.css";
 
 export default function Category() {
-  return <div>Category</div>;
+  return (
+    <>
+      <h2 className="sidebar-title">Category</h2>
+      <div className="sidebar-items">
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sneakers
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Flats
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sandals
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Heels
+        </label>
+      </div>
+    </>
+  );
 }
