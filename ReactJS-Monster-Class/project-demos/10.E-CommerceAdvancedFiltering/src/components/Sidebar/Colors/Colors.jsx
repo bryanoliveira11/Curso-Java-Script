28321e95db1efc4af5514584b9d3860865a12a61
bryan <bryan.oliveira12345@gmail.com>
2025-03-17 10:14:14 -0300

    Price and Colors

--- ReactJS-Monster-Class/project-demos/10.E-CommerceAdvancedFiltering/src/components/Sidebar/Colors/Colors.jsx
+++ ReactJS-Monster-Class/project-demos/10.E-CommerceAdvancedFiltering/src/components/Sidebar/Colors/Colors.jsx
@@ -2,5 +2,33 @@ import React from "react";
 import "./Colors.css";
 
 export default function Colors() {
-  return <div>Colors</div>;
+  return (
+    <div>
+      <h2 className="sidebar-title color-title">Colors</h2>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>All
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>Black
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>Blue
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>Red
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>White
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>Green
+      </label>
+    </div>
+  );
 }
