e5f3b8ce65c2fb512df205c6176ea822e47039c2
bryan <bryan.oliveira12345@gmail.com>
2025-03-17 11:13:44 -0300

    Creating filters for everything

--- ReactJS-Monster-Class/project-demos/10.E-CommerceAdvancedFiltering/src/App.jsx
+++ ReactJS-Monster-Class/project-demos/10.E-CommerceAdvancedFiltering/src/App.jsx
@@ -1,15 +1,75 @@
-import Nav from "./components/Navigation/Nav"; 
-import Products from "./components/Products/Products";  
-import Recommended from "./components/Recommended/Recommended";  
+import { useState } from "react";
+import Nav from "./components/Navigation/Nav";
+import Products from "./components/Products/Products";
+import Recommended from "./components/Recommended/Recommended";
 import Sidebar from "./components/Sidebar/Sidebar";
+import data from "./db/data";
 
 function App() {
+  // input filter
+  const [query, setQuery] = useState("");
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+  const filteredItemsByTitle = data.filter(
+    (item) =>
+      item.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase()) !== -1
+  );
+
+  // radio filter
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  // buttons filter
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  function filteredData(data, selected, query) {
+    let filteredItems = data;
+
+    // filtering by input (search bar)
+    if (query) {
+      filteredItems = filteredItemsByTitle;
+    }
+
+    // filtering by selected filter
+    if (selected) {
+      filteredItems = filteredItems.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredItems.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
+
+  const result = filteredData(data, selectedCategory, query);
+
   return (
     <>
-      <Sidebar/>
-      <Nav/>
-      <Recommended/>
-      <Products/>
+      <Sidebar />
+      <Nav />
+      <Recommended />
+      <Products />
     </>
   );
 }
--- ReactJS-Monster-Class/project-demos/10.E-CommerceAdvancedFiltering/src/db/data.js
+++ ReactJS-Monster-Class/project-demos/10.E-CommerceAdvancedFiltering/src/db/data.js
@@ -1,7 +1,10 @@
+import { AiFillStar } from "react-icons/ai";
+
 const data = [
   {
     img: "https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg",
     title: "Nike Air Monarch IV",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "200",
@@ -12,6 +15,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg",
     title: "Nike Air Vapormax Plus",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "200",
@@ -23,6 +27,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/51+P9uAvb1L._AC_UY695_.jpg",
     title: "Nike Waffle One Sneaker",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "200",
@@ -33,6 +38,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/71oEKkghg-L._AC_UX575_.jpg",
     title: "Nike Running Shoe",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "200",
@@ -43,6 +49,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/41M54ztS6IL._AC_SY625._SX._UX._SY._UY_.jpg",
     title: "Flat Slip On Pumps",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "200",
@@ -53,6 +60,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/71zKuNICJAL._AC_UX625_.jpg",
     title: "Knit Ballet Flat",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "50",
@@ -64,6 +72,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/61V9APfz97L._AC_UY695_.jpg",
     title: "Loafer Flats",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "50",
@@ -75,6 +84,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/71VaQ+V6XnL._AC_UY695_.jpg",
     title: "Nike Zoom Freak",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "200",
@@ -86,6 +96,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/61-cBsLhJHL._AC_UY695_.jpg",
     title: "Nike Men's Sneaker",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "200",
@@ -97,6 +108,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/81xXDjojYKS._AC_UX575_.jpg",
     title: "PUMA BLACK-OCE",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "150",
@@ -107,6 +119,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/71E75yRwCDL._AC_UY575_.jpg",
     title: "Pacer Future Sneaker",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "150",
@@ -117,6 +130,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/71jeoX0rMBL._AC_UX575_.jpg",
     title: "Unisex-Adult Super",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "150",
@@ -127,6 +141,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/61TM6Q9dvxL._AC_UX575_.jpg",
     title: "Roma Basic Sneaker",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "150",
@@ -137,6 +152,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/7128-af7joL._AC_UY575_.jpg",
     title: "Pacer Future Doubleknit",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "150",
@@ -148,6 +164,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/81xXDjojYKS._AC_UX575_.jpg",
     title: "Fusion Evo Golf Shoe",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "100",
@@ -158,6 +175,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/719gdz8lsTS._AC_UX575_.jpg",
     title: "Rainbow Chex Skate",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "100",
@@ -168,6 +186,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/71gpFHJlnoL._AC_UX575_.jpg",
     title: "Low-Top Trainers",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "100",
@@ -178,6 +197,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/71pf7VFs9CL._AC_UX575_.jpg",
     title: "Vans Unisex Low-Top",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "100",
@@ -188,6 +208,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/61N4GyWcHPL._AC_UY575_.jpg",
     title: "Classic Bandana Sneakers",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "50",
@@ -198,6 +219,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/61bncQ44yML._AC_UX695_.jpg",
     title: "Chunky High Heel",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "50",
@@ -208,6 +230,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/71czu7WgGuL._AC_UY695_.jpg",
     title: "Slip On Stiletto High Heel",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "100",
@@ -218,6 +241,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/61men05KRxL._AC_UY625_.jpg",
     title: "DREAM PAIRS Court Shoes",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "150",
@@ -228,6 +252,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg",
     title: "Nike Air Vapormax Plus",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "200",
@@ -239,6 +264,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/51PGWTXgf-L._AC_UY625_.jpg",
     title: "Low Mid Block Heels",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "200",
@@ -249,6 +275,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/616sA5XUKtL._AC_UY675_.jpg",
     title: "Chunky High Heel",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "50",
@@ -259,6 +286,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/71h5+MbEK7L._AC_UY625_.jpg",
     title: "Amore Fashion Stilettos",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "150",
@@ -269,6 +297,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/61uw5RDxKQL._AC_UY625_.jpg",
     title: "Bridal Sandals Glitter",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "50",
@@ -279,6 +308,7 @@ const data = [
   {
     img: "https://m.media-amazon.com/images/I/71yhoZP0l6L._AC_UY695_.jpg",
     title: "Wedding Prom Bridal",
+    star: <AiFillStar className="rating-star" />,
     reviews: "(123 reviews)",
     prevPrice: "$140,00",
     newPrice: "50",
