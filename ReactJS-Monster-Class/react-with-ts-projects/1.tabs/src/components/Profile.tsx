010888d201fa564db404e04e0e1aec54f07234f7
bryan <bryan.oliveira12345@gmail.com>
2025-03-20 11:59:03 -0300

    Tabs part 1

---
+++ ReactJS-Monster-Class/react-with-ts-projects/1.tabs/src/components/Profile.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { FaCamera } from "react-icons/fa";
+import Tabs from "./Tabs";
+
+export default function Profile() {
+  const [bannerUrl, setBannerUrl] = React.useState(
+    "https://placehold.co/1500x400"
+  );
+
+  const [profileUrl, setProfileUrl] = React.useState(
+    "https://placehold.co/100"
+  );
+
+  const handleBannerChange = (event: any) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      setBannerUrl(URL.createObjectURL(file));
+    }
+  };
+
+  const handleProfileChange = (event: any) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      setProfileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  return (
+    <div className="relative ml-[5rem]">
+      <div className="relative">
+        <img src={bannerUrl} alt="" className="w-full h-60 object-cover" />
+
+        <button
+          className="absolute top-2 right-2 bg-gray-800 text-white
+        p-2 rounded-full hover:bg-gray-600"
+        >
+          <label htmlFor="banner-upload" className="cursor-pointer">
+            <FaCamera size={24} />
+          </label>
+          <input
+            type="file"
+            id="banner-upload"
+            accept="image/*"
+            className="hidden"
+            onChange={handleBannerChange}
+          />
+        </button>
+      </div>
+
+      <div className="flex items-center ml-4 mt-[2rem]">
+        <img
+          src={profileUrl}
+          alt=""
+          className="h-40 w-40 object-cover rounded-full border-white relative"
+        />
+        <button
+          className="absolute ml-[4rem] z-10 mt-[9rem] bg-gray-800 text-white
+        p-2 rounded-full hover:bg-gray-600"
+        >
+          <label htmlFor="profile-upload" className="cursor-pointer">
+            <FaCamera size={24} />
+          </label>
+          <input
+            type="file"
+            id="profile-upload"
+            accept="image/*"
+            className="hidden"
+            onChange={handleProfileChange}
+          />
+        </button>
+
+        <div className="ml-4 mt-4">
+          <h1 className="text-2xl font-bold">Some Name</h1>
+          <p>1M Views</p>
+          <p className="mt-2 text-wrap w-[50rem]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+            eum omnis assumenda sint explicabo ipsam, asperiores maxime
+            quibusdam ipsa modi ipsum, aspernatur aliquid quos enim, perferendis
+            reprehenderit voluptatem repellendus! Commodi.
+          </p>
+          <button
+            className="mt-4 bg-red-600 text-white py-2 px-4 rounded
+        hover:bg-red-500 cursor-pointer"
+          >
+            Subscribe
+          </button>
+        </div>
+      </div>
+      <Tabs/>
+    </div>
+  );
+}
---
+++ ReactJS-Monster-Class/react-with-ts-projects/1.tabs/src/components/Sidebar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaHome, FaSearch, FaUser } from "react-icons/fa";
+import { IoMdSettings } from "react-icons/io";
+
+export default function Sidebar() {
+  return (
+    <>
+      <aside className="sidebar fixed top-0 left-0 h-screen w-20 bg-[#1A1C1E] text-white">
+        <ul className="p-4 flex flex-col justify-between items-center h-full">
+          <div className="top">
+            <li className="mb-2">
+              <div className="flex items-center cursor-pointer">
+                <FaHome className="mr-2 mb-3" size={18} />
+              </div>
+            </li>
+            <li className="mb-2">
+              <div className="flex items-center cursor-pointer">
+                <FaUser className="mr-2 mb-3" size={18} />
+              </div>
+            </li>
+            <li className="mb-2">
+              <div className="flex items-center cursor-pointer">
+                <FaSearch className="mr-2 mb-3" size={18} />
+              </div>
+            </li>
+          </div>
+
+          <div className="bottom">
+            <li>
+              <IoMdSettings size={18} className="cursor-pointer" />
+              <FaUser className="mt-5 cursor-pointer" size={18} />
+            </li>
+          </div>
+        </ul>
+      </aside>
+    </>
+  );
+}
