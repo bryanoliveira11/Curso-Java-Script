75f2ace02d145a7aedd04408dce3b8ea6accdd2c
bryan <bryan.oliveira12345@gmail.com>
2025-03-27 10:39:22 -0300

    filters

--- ReactJS-Monster-Class/react-with-ts-projects/3.SimpleFilteration/src/components/Table.tsx
+++ ReactJS-Monster-Class/react-with-ts-projects/3.SimpleFilteration/src/components/Table.tsx
@@ -3,14 +3,26 @@ import { data } from "../utils/data";
 import { BiSort } from "react-icons/bi";
 import { AiOutlineDown } from "react-icons/ai";
 import { BsThreeDots } from "react-icons/bs";
+import { MdSort } from "react-icons/md";
 
 const Table = () => {
   const [projects, setProjects] = React.useState(data);
   const [dropdownVisible, setDropdownVisible] = React.useState(false);
+  const [filtersVisible, setFiltersVisible] = React.useState(false);
   const [sortConfig, setSortConfig] = React.useState<{
     key: string;
     direction: string;
   } | null>(null);
+
+  const [filters, setFilters] = React.useState({
+    name: "",
+    country: "",
+    email: "",
+    project: "",
+    status: "",
+  });
+
+  const [searchQuery, setSearchQuery] = React.useState("");
 
   const sortProjects = (key: keyof (typeof data)[0]) => {
     const sortedProjects = [...projects];
@@ -35,45 +47,154 @@ const Table = () => {
     setDropdownVisible(false);
   };
 
+  const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilters({
+      ...filters,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const filteredProjects = projects.filter(
+    (project) =>
+      (searchQuery === "" ||
+        Object.values(project).some((value) =>
+          value.toLowerCase().includes(searchQuery.toLowerCase())
+        )) &&
+      (filters.name === "" ||
+        project.client.toLowerCase().includes(filters.name.toLowerCase())) &&
+      (filters.country === "" ||
+        project.country
+          .toLowerCase()
+          .includes(filters.country.toLowerCase())) &&
+      (filters.email === "" ||
+        project.email.toLowerCase().includes(filters.email.toLowerCase())) &&
+      (filters.project === "" ||
+        project.project
+          .toLowerCase()
+          .includes(filters.project.toLowerCase())) &&
+      (filters.status === "" ||
+        project.status.toLowerCase().includes(filters.status.toLowerCase()))
+  );
+
   return (
     <div className="py-4 w-[93%] ml-[5rem]">
-      <div className="relative">
-        <div className="flex items-center mb-5">
-          <button
-            onClick={() => setDropdownVisible(!dropdownVisible)}
-            className="border border-gray-700 flex items-center
+      <div className="flex relative items-center mb-5">
+        <button
+          onClick={() => setDropdownVisible(!dropdownVisible)}
+          className="border border-gray-700 flex items-center
             justify-center text-white p-2 rounded cursor-pointer"
+        >
+          <BiSort className="mr-[0.3rem]" /> Sort
+          <AiOutlineDown className="ml-2" />
+        </button>
+
+        {dropdownVisible && (
+          <div
+            className="absolute top-full left-0 mt-2 bg-gray-800
+            border-gray-700 shadow-lg"
           >
-            <BiSort className="mr-[0.3rem]" /> Sort
+            <button
+              className="block px-4 py-2 text-white w-full
+            hover:bg-gray-700 cursor-pointer"
+              onClick={() => handleSortOption("client")}
+            >
+              Client
+            </button>
+            <button
+              className="block px-4 py-2 text-white w-full
+            hover:bg-gray-700 cursor-pointer"
+              onClick={() => handleSortOption("country")}
+            >
+              Country
+            </button>
+            <button
+              className="block px-4 py-2 text-white w-full
+            hover:bg-gray-700 cursor-pointer"
+              onClick={() => handleSortOption("date")}
+            >
+              Date
+            </button>
+          </div>
+        )}
+
+        <div className="relative ml-4 w-full">
+          <button
+            onClick={() => setFiltersVisible(!filtersVisible)}
+            className="border border-gray-700 flex items-center
+        justify-center text-white p-2 rounded cursor-pointer"
+          >
+            <MdSort className="mr-[0.3rem]" /> Filters
             <AiOutlineDown className="ml-2" />
           </button>
 
-          {dropdownVisible && (
+          {filtersVisible && (
             <div
-              className="absolute top-full left-0 mt-2 bg-gray-800
-            border-gray-700 shadow-lg"
+              className="absolute top-full left-0 mt-2 bg-gray-800 border
+            border-gray-700 rounded shadow-lg p-4"
             >
-              <button
-                className="block px-4 py-2 text-white w-full
-            hover:bg-gray-700 cursor-pointer"
-                onClick={() => handleSortOption("client")}
-              >
-                Client
-              </button>
-              <button
-                className="block px-4 py-2 text-white w-full
-            hover:bg-gray-700 cursor-pointer"
-                onClick={() => handleSortOption("country")}
-              >
-                Country
-              </button>
-              <button
-                className="block px-4 py-2 text-white w-full
-            hover:bg-gray-700 cursor-pointer"
-                onClick={() => handleSortOption("date")}
-              >
-                Date
-              </button>
+              <div>
+                <label className="block text-white">Filter By Name:</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={filters.name}
+                  onClick={handleFilterChange}
+                  className="bg-gray-900 text-white rounded
+                    p-2 w-full mt-2 focus:outline-none"
+                />
+              </div>
+              <div>
+                <label className="block text-white mt-2">
+                  Filter By Country:
+                </label>
+                <input
+                  type="text"
+                  name="country"
+                  value={filters.country}
+                  onClick={handleFilterChange}
+                  className="bg-gray-900 text-white rounded
+                    p-2 w-full mt-2 focus:outline-none"
+                />
+              </div>
+              <div>
+                <label className="block text-white mt-2">
+                  Filter By Email:
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  value={filters.email}
+                  onClick={handleFilterChange}
+                  className="bg-gray-900 text-white rounded
+                    p-2 w-full mt-2 focus:outline-none"
+                />
+              </div>
+              <div>
+                <label className="block text-white mt-2">
+                  Filter By Project:
+                </label>
+                <input
+                  type="text"
+                  name="project"
+                  value={filters.project}
+                  onClick={handleFilterChange}
+                  className="bg-gray-900 text-white rounded
+                    p-2 w-full mt-2 focus:outline-none"
+                />
+              </div>
+              <div>
+                <label className="block text-white mt-2">
+                  Filter By Status:
+                </label>
+                <input
+                  type="text"
+                  name="status"
+                  value={filters.status}
+                  onClick={handleFilterChange}
+                  className="bg-gray-900 text-white rounded
+                    p-2 w-full mt-2 focus:outline-none"
+                />
+              </div>
             </div>
           )}
         </div>
