07457072e9d9e607b22e2823185afe0e5b7c64d2
bryan <bryan.oliveira12345@gmail.com>
2025-03-31 10:40:08 -0300

    Sidebar

---
+++ ReactJS-Monster-Class/react-with-ts-projects/4.EcomAdvancedFilterationSystem/src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="flex h-screen">
+        <Sidebar />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
