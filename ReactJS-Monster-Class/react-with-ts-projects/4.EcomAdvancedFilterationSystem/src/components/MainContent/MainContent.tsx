9eca0e3d1816ada9069b2a89bc8d35f10d4c3734
bryan <bryan.oliveira12345@gmail.com>
2025-04-03 12:22:12 -0300

    Ending

--- ReactJS-Monster-Class/react-with-ts-projects/4.EcomAdvancedFilterationSystem/src/components/MainContent/MainContent.tsx
+++ ReactJS-Monster-Class/react-with-ts-projects/4.EcomAdvancedFilterationSystem/src/components/MainContent/MainContent.tsx
@@ -101,7 +101,7 @@ export default function MainContent() {
   };
 
   return (
-    <section className="xl:w-[55rem] lg:w-[55rem] sm:w-[40rem] xs:w-[20rem] p-5">
+    <section className="xl:w-[55rem] lg:w-[55rem] sm:w-[40rem] xs:w-[20rem] p-5 mr-[10rem]">
       <div className="mb-5">
         <div className="flex flex-col sm:flex-row justify-between items-center">
           <div className="relative mb-5 mt-3">
